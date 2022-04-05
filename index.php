477841530dce6baa44e4b69f385ab53a14bc9828
JamesUran <101643241+JamesUran@users.noreply.github.com>
2022-04-04 21:56:12 -0500

    Sistema de registro y login
    Funcionalidad del sistema de login, basado en la implementacion de un modulo de php.

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
                 <!--Formulario de Login y registro-->
                 <div class="contenedor__login-register">
                     <!--Login-->
-                    <form action="" class="formulario__login">
+                    <form action="php/login_usuario_be.php" method = "POST" class="formulario__login">
                         <h2>Iniciar Sesión</h2>
                         <input type="text" placeholder="Correo Electronico" name = "correo">
                         <input type="password" placeholder="Contraseña" name = "contrasena">
@@ -39,7 +39,7 @@
                     </form>
 
                     <!--Register-->
-                    <form action="" class="formulario__register">
+                    <form action="php/registro_usuario_be.php" method = "POST" class="formulario__register">
                         <h2>Regístrarse</h2>
                         <input type="text" placeholder="Nombre completo" name = "nombre_completo">
                         <input type="text" placeholder="Correo Electronico" name = "correo">
